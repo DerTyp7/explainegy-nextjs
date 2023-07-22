bb44e4e6a5dc65aa38ff2c068003e124cc6b9075
Janis <janis.meister87@gmail.com>
2023-07-23 01:32:58 +0200

    add test icon

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -15,8 +15,14 @@ export default function CategoryList({ categories }: { categories: CategoryWithI
                   <div key={i} className={styles.linkContainer}>
                     <Link href={`/articles/${cat.name.toLowerCase()}`} style={{ backgroundColor: cat.color }}>
                       <div className={styles.svgContainer}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox={cat?.svg?.viewbox}>
-                          <path d={cat?.svg?.path} />
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox={cat?.svg?.viewbox.length > 0 ? cat?.svg?.viewbox : "0 0 512 512"}>
+                          <path
+                            d={
+                              cat?.svg?.path.length > 0
+                                ? cat?.svg?.path
+                                : "M0 64C0 46.3 14.3 32 32 32H88h48 56c17.7 0 32 14.3 32 32s-14.3 32-32 32V400c0 44.2-35.8 80-80 80s-80-35.8-80-80V96C14.3 96 0 81.7 0 64zM136 96H88V256h48V96zM288 64c0-17.7 14.3-32 32-32h56 48 56c17.7 0 32 14.3 32 32s-14.3 32-32 32V400c0 44.2-35.8 80-80 80s-80-35.8-80-80V96c-17.7 0-32-14.3-32-32zM424 96H376V256h48V96z"
+                            }
+                          />
                         </svg>
                       </div>
                       {cat.title}
